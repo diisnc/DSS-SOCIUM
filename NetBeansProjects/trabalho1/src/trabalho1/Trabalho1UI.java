75570fbc34b35fd14fb644ba9af83cad7a656d85
marcospgp <marcospereiradev@gmail.com>
2017-09-25 18:35:36 +0100

    Rename main method

--- NetBeansProjects/trabalho1/src/trabalho1/Trabalho1UI.java
+++ NetBeansProjects/trabalho1/src/trabalho1/Trabalho1UI.java
@@ -78,7 +78,7 @@ public class Trabalho1UI extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void showUI() {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
