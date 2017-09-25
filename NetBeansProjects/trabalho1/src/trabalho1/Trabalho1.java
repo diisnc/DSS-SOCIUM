81c1b83fc942656499ddbc17bcfa8db92ea454e2
marcospgp <marcospereiradev@gmail.com>
2017-09-25 18:35:28 +0100

    Fix indentation

--- NetBeansProjects/trabalho1/src/trabalho1/Trabalho1.java
+++ NetBeansProjects/trabalho1/src/trabalho1/Trabalho1.java
@@ -11,37 +11,36 @@ import java.text.SimpleDateFormat;
 
 public final class Trabalho1 {
 
-		private static HashMap<String, SociUM> listasocios = new HashMap<String, SociUM>();
+    private static HashMap<String, SociUM> listasocios = new HashMap<String, SociUM>();
 
 
-		public static SociUM createSociUM(String numero, String nome, String curso, int anocurso, int anopresente, String morada, ArrayList<Quota> cot) {
+    public static SociUM createSociUM(String numero, String nome, String curso, int anocurso, int anopresente, String morada, ArrayList<Quota> cot) {
 
-			SociUM newSociUM = new SociUM(numero, nome, curso, anocurso, anopresente, morada, cot);
-			listasocios.put(nome, newSociUM);
+        SociUM newSociUM = new SociUM(numero, nome, curso, anocurso, anopresente, morada, cot);
+        listasocios.put(nome, newSociUM);
 
-			return newSociUM;
-		}
-
-
-		public static void main(String[] args) {
-                    
-                    ///////////// TESTING ////////////////////////
-                    System.out.println("Testando trabalho1");
-                    
-                    Date data = new Date();
-                    Quota quota = new Quota(data,5.0);
-                    
-                    ArrayList<Quota> test = new ArrayList<Quota>();
-                    test.add(quota);
-                    
-                    SociUM diana = createSociUM("A78985", "Diana Costa", "MIEI", 3, 2017, "Rua das flores", test);
-                    diana.toString();
-                    System.out.println(diana);
-                    
-                    //////////////////////////////////////////////
-                    //////////////////////////////////////////////
-		}
+        return newSociUM;
+    }
 
 
+    public static void main(String[] args) {
 
+        ///////////// TESTING ////////////////////////
+        System.out.println("Testando trabalho1");
+
+        Date data = new Date();
+        Quota quota = new Quota(data,5.0);
+
+        ArrayList<Quota> test = new ArrayList<Quota>();
+        test.add(quota);
+
+        SociUM diana = createSociUM("A78985", "Diana Costa", "MIEI", 3, 2017, "Rua das flores", test);
+        diana.toString();
+        System.out.println(diana);
+
+        //////////////////////////////////////////////
+        //////////////////////////////////////////////
+        
+        Trabalho1UI.showUI();
+    }
 }
